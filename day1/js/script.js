cbf3db1118ccb01a6125921d498bc1275dd39ce0
peoray <peoray@yahoo.com>
2018-01-19 12:01:31 +0100

    Add countdown timer in JS

--- day1/js/script.js
+++ day1/js/script.js
@@ -5,3 +5,28 @@ function getYear() {
 }
 
 getYear();
+
+function countDown() {
+	var weddingDate = new Date("December 1, 2018 10:00:00");
+	var now = new Date();
+	var timeDiff = weddingDate.getTime() - now.getTime();
+
+	var seconds = Math.floor(timeDiff / 1000);
+	var minutes = Math.floor(seconds / 60);
+	var hours = Math.floor(minutes / 60);
+	var days = Math.floor(hours / 24);
+
+	hours %= 24;
+	minutes %= 60
+	seconds %= 60;
+
+
+
+	setTimeout(countDown, 1000);
+
+
+	console.log(days);
+	console.log(hours);
+	console.log(minutes);
+	console.log(seconds);
+}

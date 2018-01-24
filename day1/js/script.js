a89bef161a551c728c80e9d6ad4a5749ef9e53f0
peoray <peoray@yahoo.com>
2018-01-24 01:04:42 +0100

    Add back to top button functionality

--- day1/js/script.js
+++ day1/js/script.js
@@ -38,3 +38,58 @@ function changeText(e) {
 
 	
 	
+var html, body, sttbtn;
+
+	window.onload = function() {
+		html = document.documentElement;
+		body = document.body;
+		sttbtn = document.getElementById('btt');
+	}
+
+	window.onscroll = stt;
+
+	function stt() {
+		var windowInnerHeightX2 = 2 * innerHeight;
+		if (body.scrollTop > windowInnerHeightX2 || html.scrollTop > windowInnerHeightX2) {
+			sttbtn.classList.add('show');
+		}
+		else {
+			sttbtn.classList.remove('show');
+		}
+	}
+
+	function sttop(totalTime, easingPower) {
+		var timeInterval = 1;
+		var scrollTop = Math.round(body.scrollTop || html.scrollTop);
+
+		var timeLeft = totalTime;
+		var scrollByPixel = setInterval(function() {
+			var percentSpent = (totalTime - timeLeft) / totalTime;
+			if (timeLeft >= 0) {
+				var newscroll = scrollTop * (1 - ease(percentSpent, easingPower));
+
+				body.scrollTop = newscroll;
+				html.scrollTop = newscroll;
+				timeLeft--;
+			}
+
+			else {
+				clearInterval(scrollByPixel);
+			}
+		}, timeInterval);
+	}
+
+	function ease(t, power) {
+		if (t < 0.5) {
+			return  0.5 * Math.pow(2 * t, power)
+		}
+
+		else {
+			return  0.5 * ( 2 - Math.pow(2 * (1 - t), power));
+		}
+	}
+	
+	document.getElementById('btt').addEventListener('click', function() {
+		sttop(300, 3);
+	});
+	
